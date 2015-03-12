21fadc9a7fc5da13caa388c6f8c17c5279e61852
Tina Ramsey <tina.ramsey87@gmail.com>
2015-03-12 16:49:32 -0700

    change some css on the project images

--- services/ProjectsFactory.js
+++ services/ProjectsFactory.js
@@ -1,9 +1,10 @@
 portfolio.factory("ProjectsFactory", function ProjectsFactory() {
   var factory = {};
   factory.projects = [
-    { title: "Potty Break", link: "http://tinaramsey87.github.io/potty_break_js", image: "./img/poop.jpg" },
+    { title: "Potty Break", link: "http://tinaramsey87.github.io/potty_break_js", image: "./img/toilet.jpg" },
     { title: "Animal Shelter", link: "http://tinaramsey87.github.io/animal_shelter_js", image: "./img/cat.jpg"},
-    { title: "Concert Tracker", link: "https://aqueous-woodland-6951.herokuapp.com", image: "./img/concert.jpg"}
+    { title: "Concert Tracker", link: "https://aqueous-woodland-6951.herokuapp.com", image: "./img/concert.jpg"},
+    { title: "Email Client", link: "https://tinaramsey87.github.io/email_client_js", image: "./img/email.png"}
   ];
 
   return factory;
