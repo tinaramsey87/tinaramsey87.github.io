62d67ae6515a6b81cbb5848ad9183a99fa63535a
Tina Ramsey <tina.ramsey87@gmail.com>
2015-03-13 16:25:39 -0700

    start getting modals to display project details

--- controllers/ProjectsController.js
+++ controllers/ProjectsController.js
@@ -1,4 +1,10 @@
-portfolio.controller('ProjectsCtrl', function ProjectsCtrl($scope, ProjectsFactory) {
+portfolio.controller('ProjectsCtrl', function ProjectsCtrl($scope, ProjectsFactory, $stateParams, UtilitiesFactory) {
   $scope.projects = ProjectsFactory.projects;
   $scope.ProjectsFactory = ProjectsFactory;
+  $scope.project = UtilitiesFactory.findById(ProjectsFactory.projects, $stateParams.projectId)
+
+  $scope.callModal = function(project) {
+    project = $scope.project
+    $('#myModal').modal();
+  }
 });
--- services/ProjectsFactory.js
+++ services/ProjectsFactory.js
@@ -1,10 +1,10 @@
 portfolio.factory("ProjectsFactory", function ProjectsFactory() {
   var factory = {};
   factory.projects = [
-    { title: "Potty Break", link: "http://tinaramsey87.github.io/potty_break_js", image: "./img/toilet.jpg" },
-    { title: "Animal Shelter", link: "http://tinaramsey87.github.io/animal_shelter_js", image: "./img/cat.jpg"},
-    { title: "Concert Tracker", link: "https://aqueous-woodland-6951.herokuapp.com", image: "./img/concert.jpg"},
-    { title: "Email Client", link: "https://tinaramsey87.github.io/email_client_js", image: "./img/email.png"}
+    { title: "Potty Break", link: "http://tinaramsey87.github.io/potty_break_js", image: "./img/toilet.jpg", id: 1 },
+    { title: "Animal Shelter", link: "http://tinaramsey87.github.io/animal_shelter_js", image: "./img/cat.jpg", id: 2 },
+    { title: "Concert Tracker", link: "https://aqueous-woodland-6951.herokuapp.com", image: "./img/concert.jpg", id: 3 },
+    { title: "Email Client", link: "https://tinaramsey87.github.io/email_client_js", image: "./img/email.png", id: 4 }
   ];
 
   return factory;
