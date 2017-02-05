8b3acb7c0033786941b5f4898392674b05af21b5
Tina Ramsey <tina.ramsey87@gmail.com>
2017-02-04 19:26:45 -0800

    removed quotes and associated controller, twitter link and skill lists

--- controllers/ProjectsController.js
+++ controllers/ProjectsController.js
@@ -1,13 +1,8 @@
-  // $scope.callModal = function(project) {
-  //   project = $scope.project
-  //   $('#myModal').modal();
-  // }
 
 portfolio.controller('ProjectsCtrl', function ProjectsCtrl($scope, $modal, ProjectsFactory, $stateParams, UtilitiesFactory) {
 
   $scope.projects = ProjectsFactory.projects;
   $scope.ProjectsFactory = ProjectsFactory;
-  // $scope.project = UtilitiesFactory.findById(ProjectsFactory.projects, $stateParams.projectId)
 
   $scope.open = function(project) {
 
--- services/ProjectsFactory.js
+++ services/ProjectsFactory.js
@@ -4,8 +4,7 @@ portfolio.factory("ProjectsFactory", function ProjectsFactory() {
     { title: "Potty Break", info: "Potty Break is a text-based game in which the player must find a bathroom in a strange office building or risk unpleasant consequences.", tech: "AngularJS", link: "http://tinaramsey87.github.io/potty_break_js", image: "./img/toilet.jpg", id: 1 },
     { title: "Animal Shelter", info: "A web app where people can 'adopt' and 'abandon' pets, view adoptable pets and search by pet name or type.", tech: "AngularJS", link: "http://tinaramsey87.github.io/animal_shelter_js", image: "./img/cat.jpg", id: 2 },
     { title: "Concert Tracker", info: "Concert tracker lets users add the names of bands and concert venues and keep track of which bands have played which venues.", tech: "Ruby/Sinatra", link: "https://aqueous-woodland-6951.herokuapp.com", image: "./img/concert.jpg", id: 3 },
-    { title: "Email Client", info: "This is an email client where users can 'send' email, with support for multiple user inboxes.", tech: "EmberJS", link: "https://tinaramsey87.github.io/email_client_js", image: "./img/email.png", id: 4 },
-    { title: "Undead Journal", info: "A blogging app that lets users sign in and create posts, as well as comment on posts.", tech: "Ruby on Rails", link: "https://quiet-lake-9545.herokuapp.com", image: "./img/journal.jpg", id: 5 }
+    { title: "Undead Journal", info: "A blogging app that lets users sign in and create posts, as well as comment on posts.", tech: "Ruby on Rails", link: "https://quiet-lake-9545.herokuapp.com", image: "./img/journal.jpg", id: 4 }
   ];
 
   return factory;
